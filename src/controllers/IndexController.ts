977b466d8e3990071550da5f376bf7709e9d1948
dimaslz <me@dimaslz.io>
2022-02-20 19:14:26 +0100

    chore: update return data

--- src/controllers/IndexController.ts
+++ src/controllers/IndexController.ts
@@ -2,9 +2,9 @@ import { NextFunction, Request, Response, Router } from 'express';
 export const IndexController: Router = Router();
 
 IndexController.get('/', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        res.status(200).send({ data: 'Hello from Ornio AS!' });
-    } catch (e) {
-        next(e);
-    }
+  try {
+    res.status(200).send({ data: 'Hello 🙋‍♂️!' });
+  } catch (e) {
+    next(e);
+  }
 });
